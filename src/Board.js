3580f1f45562f7db9545b7a86e0791484a812bf0
Hank Yates <hankyates@gmail.com>
2014-02-16 21:04:49 +0000

    adding Test for Board constructor

--- src/Board.js
+++ src/Board.js
@@ -1,3 +1,11 @@
-module.exports = function() {
+function Board(rows){
+  this.arr = [];
 
+  for (var i = 0; i < rows; i++) {
+    this.arr.push(Math.round(Math.random()));
+  };
+
+  return this;
 };
+
+module.exports = Board;
--- test/board_test.js
+++ test/board_test.js
@@ -3,12 +3,17 @@ var expect = require('chai').expect,
 
 describe('Board', function() {
   beforeEach(function() {
-    this.board = new Board();
+    this.size = 10;
+    this.board = new Board(this.size);
   });
 
   describe(' constructor', function() {
-    it('should be an object', function() {
+    it('should return an object', function() {
       expect(typeof this.board === 'object').to.be.true;
+    });
+
+    it('should create an array with number or rows passed in', function() {
+      expect(this.board.arr.length).to.equal(this.size);
     });
   });
 
