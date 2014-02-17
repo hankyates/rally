d04b55222aca30e0bb9c8256d13eb79db9cfa301
Hank Yates <hankyates@gmail.com>
2014-02-17 04:40:12 +0000

    Board#get returns false if out of bounds. Board#nextGeneration implementation

--- src/Board.js
+++ src/Board.js
@@ -15,36 +15,72 @@ function Board(rows){
 
 Board.prototype = {
   get: function(coordinates) {
+    var x, y;
     if (coordinates instanceof Cell){
-      return this.arr[coordinates.y][coordinates.x];
+      x = coordinates.x;
+      y = coordinates.y;
+    } else {
+      x = coordinates[0];
+      y = coordinates[1];
     }
 
-    return this.arr[coordinates[1]][coordinates[0]];
+    if (x > -1 && x < this.arr.length){
+      if (y > -1 && y < this.arr.length){
+        return this.arr[y][x];
+      }
+    }
+    return false;
   },
   noOfLivingNeighboors: function(coordinates) {
-    var livingNeightboors = 0,
-        cell = this.get(coordinates);
+    var livingNeighboors = 0,
+        cell = this.get(coordinates),
+        up = cell.up(),
+        up_right = cell.up_right(),
+        right = cell.right(),
+        down_right = cell.down_right(),
+        down = cell.down(),
+        down_left = cell.down_left(),
+        left = cell.left(),
+        up_left = cell.up_left();
 
-    if(this.get(cell.up()).value)
-      livingNeightboors++;
-    if(this.get(cell.up_right()).value)
-      livingNeightboors++;
-    if(this.get(cell.right()).value)
-      livingNeightboors++;
-    if(this.get(cell.down_right()).value)
-      livingNeightboors++;
-    if(this.get(cell.down()).value)
-      livingNeightboors++;
-    if(this.get(cell.down_left()).value)
-      livingNeightboors++;
-    if(this.get(cell.left()).value)
-      livingNeightboors++;
-    if(this.get(cell.up_left()).value)
-      livingNeightboors++;
+    if(up && this.get(up).value)
+      livingNeighboors++;
+    if(up_right && this.get(up_right).value)
+      livingNeighboors++;
+    if(right && this.get(right).value)
+      livingNeighboors++;
+    if(down_right && this.get(down_right).value)
+      livingNeighboors++;
+    if(down && this.get(down).value)
+      livingNeighboors++;
+    if(down_left && this.get(down_left).value)
+      livingNeighboors++;
+    if(left && this.get(left).value)
+      livingNeighboors++;
+    if(up_left && this.get(up_left).value)
+      livingNeighboors++;
 
-    return livingNeightboors;
+    return livingNeighboors;
   },
   nextGeneration: function() {
+    var dyingCells = [],
+        reanimatingCells = [];
+
+    this.arr.forEach(function(row) {
+      row.forEach(function(cell) {
+        var livingNeighboors = this.noOfLivingNeighboors(cell);
+
+        if (livingNeighboors < 2 || livingNeighboors > 3) {
+          dyingCells.push(cell);
+        } else if (cell.isDead() && livingNeighboors === 3) {
+          reanimatingCells.push(cell);
+        }
+
+      }, this);
+    }, this);
+
+    dyingCells.forEach(function(cell) {cell.kill()});
+    reanimatingCells.forEach(function(cell) {cell.reanimate()});
   },
   print: function() {
     for (var y = 0; y < this.arr.length; y++) {
